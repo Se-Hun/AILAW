668066659ccbeac3ae85258884ff64182c371be4
Se-Hun <sehunhu5247@gmail.com>
2020-11-04 00:46:50 +0900

    add to-do list

--- data/excel_to_tag.py
+++ data/excel_to_tag.py
@@ -40,20 +40,32 @@ import numpy as np
 #
 #         fp.write("------------------------------\n")
 
-def build_tag(fn, tags):
-    law_df = pd.read_excel(fn, sheet_name = 'Sheet1')
+def build_tag(fns, ner_tags):
+    in_fn = fns['input']
+    to_fn = fns['output']
 
-    answer_columns = law_df.columns[3:]
-    for idx, col in enumerate(answer_columns):
+    df = pd.read_excel(in_fn, sheet_name = '시트1')
+
+    #TODO : (1) 필요한 데이터 excel에서 파싱하기
+    neccessary_columns = df.columns[3:]
+    for idx, col in enumerate(neccessary_columns):
         if idx % 2 == 0:
-            law_df[col] = law_df[col].apply(lambda x: len(x))
+            df[col] = df[col].apply(lambda x: len(x))
 
-    law_df['인덱스들'] = "[" + law_df[answer_columns].apply(lambda x: ",".join(x.values.astype(str)), axis=1) + "]"
+    df['인덱스들'] = "[" + df[neccessary_columns].apply(lambda x: ",".join(x.values.astype(str)), axis=1) + "]"
 
+    # TODO : (2) 문장 splitter 적용하기
+
+    # TODO : (3) 각 문장에 태그 부착하기
+
+    # TODO : (4) Text File에 저장 --> 판례별로 구분자 "------" 넣어주기
 
 if __name__ == '__main__':
-    fn = os.path.join("./", "data", "original", "law.xlsx")
+    fns = {
+        "input": os.path.join("./", "original", "law.xlsx"),
+        "output": os.path.join("./", "run", "ner_tag.txt")
+    }
 
-    tags = ["victim.who", "crime.when", "crime.where", "crime.what", "victim.age", "victim.attacker.relation"]
+    ner_tags = ["victim.who", "crime.when", "crime.where", "crime.what", "victim.age", "victim.attacker.relation"]
 
-    build_tag(fn, tags)
+    build_tag(fns, ner_tags)
