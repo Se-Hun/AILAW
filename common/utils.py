7c370d11e07c218c681cee812f76099065fc3800
Se-Hun <sehunhu5247@gmail.com>
2020-11-09 00:13:56 +0900

    utility code

--- common/utils.py
+++ common/utils.py
@@ -11,3 +11,18 @@ def exist_dir(dir_name):
     else:
         return True
 
+## -------------------------------------- Pytorch utilities --------------------------------------- ##
+import torch
+
+def is_gpu_available():
+    return torch.cuda.is_available()
+
+
+def load_model(model_fn, map_location=None):
+    if map_location:
+        return torch.load(model_fn, map_location=map_location)
+    else:
+        if torch.cuda.is_available():
+            return torch.load(model_fn)
+        else:
+            return torch.load(model_fn, map_location='cpu')
